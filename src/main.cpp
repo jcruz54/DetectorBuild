86fcad32eed77580ecb4335ded0556a70775986b
JReyes54 <stacruzjulian54@gmail.com>
2020-01-15 22:07:31 -0800

    Added project files

---
+++ src/main.cpp
@@ -0,0 +1,98 @@
+#include <Arduino.h>
+
+int tempPin = A0;
+int sensorInput;
+double currentTemp;
+double currentVoltage;
+
+/* 
+ *                              accuracyOffset: An Explanation
+ * 
+ *                             DEFAULT VALUE IS ZERO-POINT-ZERO.
+ *              DO NOT CHANGE THIS VALUE UNLESS ACTUAL TEMPERATURE IS DIFFERENT
+ *                         FROM THE TEMPERATURE OUTPUT FROM THE DEVICE.
+ * 
+ *              If there is a difference, then set this value to the difference
+ *                          between the DEVICE TEMP and ACTUAL TEMP.
+ * 
+ *           * accuracyOffset = (Temperature from Device) - (Actual Temperature) *
+ *
+ */
+double accuracyOffset = 0.0; 
+
+// REMEMBER TO CHANGE TEMP RANGE VALUES BEFORE USING
+// ADJUST LED PIN NUMBERS IF NECESSARY
+int redLED = 8;
+double redMin = 18.00;
+double redMax = 22.99;
+bool isInRedRange;
+
+int greenLED = 9;
+double greenMin = 23.0;
+double greenMax = 27.99;
+bool isInGreenRange;
+
+int blueLED = 10;
+double blueMin = 28.00;
+double blueMax = 30.00;
+bool isInBlueRange;
+
+bool checkIsInTempRange(double tempMin, double tempMax, double currentTemp) {
+  /* 
+   * Checks if current temperature is within the range of min and max values.
+   */ 
+  if (currentTemp > tempMin && currentTemp < tempMax) return true;
+  else return false;
+}
+
+void toggleLED(int ledPin, bool isInRange) {
+  /*
+   * Turns on an LED if the temperature is within the LEDs respective range.
+   */
+  if (isInRange) digitalWrite(ledPin, HIGH);
+  else digitalWrite(ledPin, LOW);
+}
+
+double convertInputToDegrees(double input) {
+  /*
+   * Converts temperature sensor input into degrees celsius.
+   */
+  return (((input / 1024) * 5) - 0.5) * 100;
+}
+
+double convertInputToVoltage(double input) {
+  /*
+   * Converts temperature sensor input into volts.
+   */
+  return (input / 1024) * 5;
+}
+
+void setup() {
+  pinMode(redLED, OUTPUT); //pinMode(pinNumber, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorInput = (double)analogRead(tempPin);
+  currentTemp = convertInputToDegrees(sensorInput) - accuracyOffset;
+  currentVoltage = convertInputToVoltage(sensorInput);
+  isInRedRange = checkIsInTempRange(redMin, redMax, currentTemp);
+  isInGreenRange = checkIsInTempRange(greenMin, greenMax, currentTemp);
+  isInBlueRange = checkIsInTempRange(blueMin, blueMax, currentTemp);
+
+  toggleLED(redLED, isInRedRange);
+  toggleLED(greenLED, isInGreenRange);
+  toggleLED(blueLED, isInBlueRange);
+
+  // Serial Output: "Temperature: NN.NN°C"
+  Serial.print("Temperature: ");
+  Serial.print(currentTemp);
+  Serial.println("°C");
+
+  Serial.print("Voltage: ");
+  Serial.println(currentVoltage);
+
+  delay(500);
+}
